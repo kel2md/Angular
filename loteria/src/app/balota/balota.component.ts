05aae43846687ef9d43c040d216baba4c00f29fe
kel2md <44475577+kel2md@users.noreply.github.com>
2021-01-29 11:15:54 -0500

    Clase
    Cambios en Loteria

--- loteria/src/app/balota/balota.component.ts
+++ loteria/src/app/balota/balota.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 })
 export class BalotaComponent implements OnInit {
   //Recibe Valor del Componente padre
-  @Input() valor : number;
+  @Input() valor : number; 
   
   constructor() { }
 
